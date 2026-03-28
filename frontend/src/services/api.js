8a7ccd6ea0ea0280088f59aa0a1ba1ec2974809b
P-1256 <158600496+P-1256@users.noreply.github.com>
2026-03-28 19:43:40 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/smarttracker';
+const BASE_URL = 'https://smart-exp-tracker.vercel.app/smarttracker';
 
 const api = axios.create({
   baseURL: BASE_URL,
